c3a274ccc1527bd8417b526cb8b37cd0e112ea70
Jean-Martin Archer <jm@jmartin.ca>
2020-12-14 09:37:51 -0800

    setup, add theme files

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="0.0.0",
     description="",
     zip_safe=False,
-    packages=find_packages(),
-    install_requires=['pyaml'],
+    packages=["sphinx_guru_builder"],
+    package_data={"sphinx_guru_builder": ["theme/*"]},
+    install_requires=["pyaml"],
 )
